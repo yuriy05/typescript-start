4a4ee5dc120a8e074be67f0e34b1da26f1becc88
yuriy05 <dilayyura@gmail.com>
2023-12-21 23:32:55 +0200

    Hello TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //My first object type written in TypeScript
+  const list: {name: string; age: number; isAdmin?: boolean} = {
+    name: "Lolita",
+    age: 25,
+    isAdmin: true,
+  }
+
+  //My first array type written in TypeScript
+  const array: (string)[] = ["John", "Paul", "Armin"]
+
+  //My first set type written in TypeScript
+  const set: Set<string> = new Set()
+  set.add("Paul")
+  
+  //My first function written in TypeScript
+  function writeIntoTheConsole(message: {}):void {
+    console.log(message)
+  }
+  writeIntoTheConsole(list)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{list.name}</p>
+        <p>{array.sort().join(" ")}</p>  
+        <p>{set}</p>
       </header>
     </div>
   );
